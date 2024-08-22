4608ca581347204d7796d9002ddec8981c131956
Endrew Sousa <endrew.developer@gmail.com>
2024-08-21 22:32:44 -0300

    atualizada para 21-8

--- exercicio_bikcraft/bikcraft/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Bike, Contados, Lojas, Pessoas, DetalheBikes
-admin.site.register(Bike)
-admin.site.register(Contados)
-admin.site.register(Lojas)
-admin.site.register(Pessoas)
-admin.site.register(DetalheBikes)
