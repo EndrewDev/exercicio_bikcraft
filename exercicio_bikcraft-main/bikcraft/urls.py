3609c47e62bda490cc0fd7f4ddd4f0b5c4e196a9
Endrew <endrew.developer@gmail.com>
2024-09-14 12:12:44 -0300

    Já mudei tudo para classes, mas só está tanto erro do site, só aparece o erro 4003. acho que é esse número

--- exercicio_bikcraft-main/bikcraft/urls.py
+++ exercicio_bikcraft-main/bikcraft/urls.py
@@ -9,24 +9,24 @@ urlpatterns = [
     path('sobre/', views.SobreView.as_view(), name='pagina-sobre'),
 
     # produtos
-    path('cadastra_produtos/', views.cadastra_produtos, name='pagina-cadastraprodutos'),
-    path('produtos/', views.produtos, name='pagina-produtos'),
-    path('produtos/atualizar/<int:pk>/', views.atualizado_produtos, name='pagina-atualizado'),
-    path('produto/deleta/<int:id>/', views.deleta_produto, name='deleta-produto'),
+    path('cadastra_produtos/', views.CadastraProdutoView.as_view(), name='pagina-cadastraprodutos'),
+    path('produtos/', views.ListProdutosView.as_view(), name='pagina-produtos'),
+    path('produtos/atualizar/<int:pk>/', views.UpdateProdutoView.as_view(), name='pagina-atualizado'),
+    path('produto/deleta/<int:id>/', views.DeleteProdutoView.as_view(), name='deleta-produto'),
 
     # lojas
-    path('cadastra_lojas/', views.cadastra_loja, name='cadastra-loja'),
-    path('lojas/', views.lojas, name='pagina-loja'),
-    path('atualiza_lojas/<int:id>/', views.atualizado_lojas, name='atualiza-lojas'),
-    path('deleta_lojas/<int:id>/', views.deleta_lojas, name='deleta-lojas'),
+    path('cadastra_lojas/', views.CadastraLojaView.as_view(), name='cadastra-loja'),
+    path('lojas/', views.ListLojasView.as_view(), name='pagina-loja'),
+    path('atualiza_lojas/<int:id>/', views.UpdateLojaView.as_view(), name='atualiza-lojas'),
+    path('deleta_lojas/<int:id>/', views.DeleteLojaView.as_view(), name='deleta-lojas'),
 
     # vendedores
-    path('cadastra_pessoas/', views.cadastra_pessoas, name='cadastra-pessoas'),
-    path('vendedores/', views.vendedores, name='pagina-vendedores'),
-    path('atualiza_pessoas/<int:id>/', views.atualiza_pessoas, name='pessoas-atualiza'),
-    path('deleta_pessoas/<int:id>/', views.deleta_pessoas, name='deleta-pessoas'),
+    path('cadastra_pessoas/', views.CadastraVendedorView.as_view(), name='cadastra-pessoas'),
+    path('vendedores/', views.ListVendedorView.as_view(), name='pagina-vendedores'),
+    path('atualiza_pessoas/<int:id>/', views.UpdateVendedorView.as_view(), name='pessoas-atualiza'),
+    path('deleta_pessoas/<int:id>/', views.DeleteVendedorView.as_view(), name='deleta-pessoas'),
     
     # contados
-    path('enviado/', views.enviado, name='pagina-enviado'),
-    path('contados/', views.contados, name='pagina-contados'),
+    path('enviado/', views.EnviadoView.as_view(), name='pagina-enviado'),
+    path('contados/', views.ContadosView.as_view(), name='pagina-contados'),
 ]
--- exercicio_bikcraft-main/bikcraft/views.py
+++ exercicio_bikcraft-main/bikcraft/views.py
@@ -3,170 +3,180 @@ from .forms import BikeModelForm, ContadosModelForm, LojasModelForm, PessoasMode
 from .models import Bike, Contados, Lojas, Pessoas #DetalheBikes
 from django.contrib.auth.decorators import login_required
 from django.views import View
-from django.views.generic import ListView, DeleteView, DetailView, CreateView, TemplateView, UpdateView
+#from django.views.generic import ListView, DeleteView, DetailView, CreateView, TemplateView, UpdateView
 
 # home class:
-class HomeView(TemplateView):
-    template_name = 'bike.html'
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'bike.html')
 
 # sobre:
-class SobreView(TemplateView):
-    template_name = 'sobre.html'
+class SobreView(View):
+    def get(self, request):
+        return render(request, 'sobre.html')
 
 # cadastra produtos:
-@login_required
-def cadastra_produtos(request):
+class CadastraProdutoView(View): 
+    @login_required
+    def cadastra_produtos(self, request):
 
-    user = request.user
-    if user.is_authenticated:
+        user = request.user
+        if user.is_authenticated:
 
-        if request.method == 'POST':
-            bike_form = BikeModelForm(request.POST, request.FILES)
-            if bike_form.is_valid():
-                bike_form.save()
-                return redirect('pagina-produtos')
+            if request.method == 'POST':
+                bike_form = BikeModelForm(request.POST, request.FILES)
+                if bike_form.is_valid():
+                    bike_form.save()
+                    return redirect('pagina-produtos')
+            else:
+                bike_form = BikeModelForm()
+                return render(request, 'cadastra_produtos.html', {'form': bike_form})
         else:
-            bike_form = BikeModelForm()
-            return render(request, 'cadastra_produtos.html', {'form': bike_form})
-    else:
-        return redirect('pagina-cadastraprodutos')
-        
-# Listas dos produtos:
-class ListasProdutoView(ListView):
-    def get(self, request):
-        bikes = bike.objects.all()
-        return render(request, 'produto.html', {'bikes': bikes})
-
-# recebe as informação para html:
-def produtos(request):
-    bike = Bike.objects.all()
-    search = request.GET.get('search')
-    if search:
-        bike = bike.filter(modelo__icontains=search)
-    return render(request, 'produtos.html', {'produtos': bike})
-
-# atualiza produto:
-def atualizado_produtos(request, pk):
-    bike = get_object_or_404(Bike, pk=pk)
-    print(request.POST)
-    if request.method == 'POST':
-        form = BikeModelForm(request.POST, instance=bike)
-        if form.is_valid():
-            form.save()
-            return redirect('pagina-produtos')
-    else:
-        form = BikeModelForm(instance=bike)
-    return render(request, 'cadastra_produtos_atualizar.html', {'form': form, 'bike': bike})
-
-# deleta produto:
-def deleta_produto(request, id):
-    deleta_produtos = get_object_or_404(Bike, pk=id)
-    print(deleta_produtos)
-    deleta_produtos.delete()
-    return redirect('pagina-produtos')
-
-# contado:
-def contados(request):
-    if request.method == "POST":
-        contados_form = ContadosModelForm(request.POST, request.FILES)
-        if contados_form.is_valid():
-            contados_form.save()
-            return redirect('pagina-enviado')
-    else:
-        contados_form = ContadosModelForm()
-    return render(request, 'contados.html', {'contados': contados_form})
-
-# enviado com sucesso do contado:
-def enviado(request):
-    contados = Contados.objects.all()
-    return render(request, 'enviado.html', {'contados': contados})
-
-# cadastra loja:
-@login_required
-def cadastra_loja(request):
-    if request.method == 'POST':
-        lojas_form = LojasModelForm(request.POST, request.FILES)
-        if lojas_form.is_valid():
-            lojas_form.save()
-            return redirect('pagina-loja')
-    else:
-        lojas_form = LojasModelForm()
-    return render(request, 'cadastra_lojas.html', {'lojas_form': lojas_form})
-
-# recebe as informação para html:
-def lojas(request):
-    lojas = Lojas.objects.all()
-    search = request.GET.get('search')
-    if search:
-        lojas = lojas.filter(modelo__icontains=search)
-    return render(request, 'lojas.html', {'lojas': lojas})
-
-# atualiza loja:
-def atualizado_lojas(request, id):
-    bike = get_object_or_404(Bike, id=id)
-
-    if request.method == 'POST':
-        modelo = request.POST.get('modelo')
-        preco = request.POST.get('preco')
-        descricao = request.POST.get('descricao')
-        foto = request.POST.get('foto')
-
-        if len(modelo) > 0:
-            bike.modelo = modelo
-        if len(preco) > 0:
-            bike.preco = preco
-        if len(descricao) > 0:
-            bike.descricao = descricao
-        if len(foto) > 0:
-            bike.foto = foto
-        bike.save()
-        return redirect('pagina-loja')
-    return render(request, 'cadastra_lojas.html', {'bike': bike})
-
-# deleta loja:
-def deleta_lojas(request, id):
-    deleta_lojas = get_object_or_404(Lojas, id=id)
-    deleta_lojas.delete()
-    return redirect('pagina-loja')
-
-#cadstra vendedor:
-@login_required
-def cadastra_pessoas(request):
-    if request.method == 'POST':
-        pessoas = PessoasModelForm(request.POST, request.FILES)
-        if pessoas.is_valid():
-            pessoas.save()
-            return redirect('pagina-vendedores')
-    else:
-        pessoas = PessoasModelForm()
-    return render(request, 'pessoas.html', {'pessoas': pessoas})
+            return redirect('pagina-cadastraprodutos')
     
 # recebe as informação para html:
-def vendedores(request):
-    informacao_pessoas = Pessoas.objects.all()
-    return render(request, 'vendedores.html', {'vendedores': informacao_pessoas})
+class ListProdutosView(View):
+    def produtos(self, request):
+        bike = Bike.objects.all()
+        search = request.GET.get('search')
+        if search:
+            bike = bike.filter(modelo__icontains=search)
+        return render(request, 'produtos.html', {'produtos': bike})
+
+# atualiza produto:
+class UpdateProdutoView(View):
+    def atualizado_produtos(self, request, pk):
+        bike = get_object_or_404(Bike, pk=pk)
+        print(request.POST)
+        if request.method == 'POST':
+            form = BikeModelForm(request.POST, instance=bike)
+            if form.is_valid():
+                form.save()
+                return redirect('pagina-produtos')
+        else:
+            form = BikeModelForm(instance=bike)
+        return render(request, 'cadastra_produtos_atualizar.html', {'form': form, 'bike': bike})
+
+# deleta produto:
+class DeleteProdutoView(View):
+    def deleta_produto(self, request, id):
+        deleta_produtos = get_object_or_404(Bike, pk=id)
+        print(deleta_produtos)
+        deleta_produtos.delete()
+        return redirect('pagina-produtos')
+
+# contado:
+class ContadosView(View):
+    def contados(self, request):
+        if request.method == "POST":
+            contados_form = ContadosModelForm(request.POST, request.FILES)
+            if contados_form.is_valid():
+                contados_form.save()
+                return redirect('pagina-enviado')
+        else:
+            contados_form = ContadosModelForm()
+        return render(request, 'contados.html', {'contados': contados_form})
+
+# enviado com sucesso do contado:
+class EnviadoView(View):
+    def enviado(self, request):
+        contados = Contados.objects.all()
+        return render(request, 'enviado.html', {'contados': contados})
+
+# cadastra loja:
+class CadastraLojaView(View):
+    @login_required
+    def cadastra_loja(self, request):
+        if request.method == 'POST':
+            lojas_form = LojasModelForm(request.POST, request.FILES)
+            if lojas_form.is_valid():
+                lojas_form.save()
+                return redirect('pagina-loja')
+        else:
+            lojas_form = LojasModelForm()
+        return render(request, 'cadastra_lojas.html', {'lojas_form': lojas_form})
+
+# recebe as informação para html:
+class ListLojasView(View):
+    def lojas(self, request):
+        lojas = Lojas.objects.all()
+        search = request.GET.get('search')
+        if search:
+            lojas = lojas.filter(modelo__icontains=search)
+        return render(request, 'lojas.html', {'lojas': lojas})
+
+# atualiza loja:
+class UpdateLojaView(View):
+    def atualizado_lojas(self, request, id):
+        bike = get_object_or_404(Bike, id=id)
+
+        if request.method == 'POST':
+            modelo = request.POST.get('modelo')
+            preco = request.POST.get('preco')
+            descricao = request.POST.get('descricao')
+            foto = request.POST.get('foto')
+
+            if len(modelo) > 0:
+                bike.modelo = modelo
+            if len(preco) > 0:
+                bike.preco = preco
+            if len(descricao) > 0:
+                bike.descricao = descricao
+            if len(foto) > 0:
+                bike.foto = foto
+            bike.save()
+            return redirect('pagina-loja')
+        return render(request, 'cadastra_lojas.html', {'bike': bike})
+
+# deleta loja:
+class DeleteLojaView(View):
+    def deleta_lojas(self, request, id):
+        deleta_lojas = get_object_or_404(Lojas, id=id)
+        deleta_lojas.delete()
+        return redirect('pagina-loja')
+
+#cadstra vendedor:
+class CadastraVendedorView(View):
+    @login_required
+    def cadastra_pessoas(self, request):
+        if request.method == 'POST':
+            pessoas = PessoasModelForm(request.POST, request.FILES)
+            if pessoas.is_valid():
+                pessoas.save()
+                return redirect('pagina-vendedores')
+        else:
+            pessoas = PessoasModelForm()
+        return render(request, 'pessoas.html', {'pessoas': pessoas})
+    
+# recebe as informação para html:
+class ListVendedorView(View):
+    def vendedores(self, request):
+        informacao_pessoas = Pessoas.objects.all()
+        return render(request, 'vendedores.html', {'vendedores': informacao_pessoas})
 
 #atualiza vendedor:
-def atualiza_pessoas(request, id):
-    pessoas_atualiza = get_object_or_404(Pessoas, id=id)
+class UpdateVendedorView(View):
+    def atualiza_pessoas(self, request, id):
+        pessoas_atualiza = get_object_or_404(Pessoas, id=id)
 
-    if request.method == 'POST':
-        nome = request.POST.get('nome')
-        cpf = request.POST.get('cpf')
-        opcao_lojas = request.POST.get('opcao_lojas')
+        if request.method == 'POST':
+            nome = request.POST.get('nome')
+            cpf = request.POST.get('cpf')
+            opcao_lojas = request.POST.get('opcao_lojas')
 
-        if len(nome) > 0:
-            pessoas_atualiza.nome = nome
-        if len(cpf) > 0:
-            pessoas_atualiza.cpf = cpf
-        if len(opcao_lojas) > 0:
-            pessoas_atualiza.opcao_lojas = opcao_lojas
-        pessoas_atualiza.save()
-        return redirect('pagina-vendedores')
-    return render(request, 'pessoas.html', {'vendeodres': pessoas_atualiza})
+            if len(nome) > 0:
+                pessoas_atualiza.nome = nome
+            if len(cpf) > 0:
+                pessoas_atualiza.cpf = cpf
+            if len(opcao_lojas) > 0:
+                pessoas_atualiza.opcao_lojas = opcao_lojas
+            pessoas_atualiza.save()
+            return redirect('pagina-vendedores')
+        return render(request, 'pessoas.html', {'vendeodres': pessoas_atualiza})
 
 # deleta vendedor:
-def deleta_pessoas(request, id):
-    pessoas = get_object_or_404(Pessoas, id=id)
-    pessoas.delete()
-    return redirect('pagina-vendedores')
+class DeleteVendedorView(View):
+    def deleta_pessoas(self, request, id):
+        pessoas = get_object_or_404(Pessoas, id=id)
+        pessoas.delete()
+        return redirect('pagina-vendedores')
